10b908d9695bd2a3bab67f7a01b517e039ed1cc3
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-02-14 21:05:56 +0330

    defining and implementing fibonacci handler

--- main/socket_programming_approach/server.py
+++ main/socket_programming_approach/server.py
@@ -2,6 +2,7 @@
 
 # import relevant modules
 from socket import *
+from ..fib import fibonacci
 
 
 def fibonacci_server(address):
@@ -12,4 +13,17 @@ def fibonacci_server(address):
     while True:
         client, addr = sock.accept()
         print(f"connection, {addr}")
-        fib_handeler(client)
+        fib_handler(client)
+
+
+def fib_handler(client):
+    while True:
+        req = client.recv(100)
+        if not req:
+            break
+        else:
+            n = int(req)
+            result = fibonacci(n)
+            resp = str(result).encode("ascii") + b"\n"
+            client.send(resp)
+    print("closed")
