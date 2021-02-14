bb7060ebca4a0b4de139193459750974046da6f8
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-02-14 20:59:19 +0330

    defining and implementing server func

--- main/socket_programming_approach/server.py
+++ main/socket_programming_approach/server.py
@@ -2,3 +2,14 @@
 
 # import relevant modules
 from socket import *
+
+
+def fibonacci_server(address):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.bind(address)
+    sock.listen(5)
+    while True:
+        client, addr = sock.accept()
+        print(f"connection, {addr}")
+        fib_handeler(client)
